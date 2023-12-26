a5e73150d5debc1efa334afb7ddc12a76eab1c90
Orçun ÖZDİL <37013255+Oozdil@users.noreply.github.com>
2023-12-26 16:19:04 +0300

    Add project files.

---
+++ MonkeyFinder/MonkeyUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MonkeyFinder
+{
+    /// <summary>
+    /// Interaction logic for MonkeyUserControl.xaml
+    /// </summary>
+    public partial class MonkeyUserControl : UserControl
+    {
+        public MonkeyUserControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        public string Image { get; set; }
+        public string MonkeyName { get; set; }
+        public string Location { get; set; }
+    }
+}
